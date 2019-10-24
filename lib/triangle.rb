bdb6668389e402ed6dd49b8fbc81476df5fd98d9
Riley Moore <rilemoore@gmail.com>
2019-10-24 21:54:34 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,8 +10,11 @@ class Triangle
   
   def kind
     if(side1 == side2 && side2 == side3 && side3 == side1 && side1 > 0 && side2 > 0 && side3 > 0)
-      :equilateral
+      return :equilateral
     end
+    
+    if(side1 == side2 )
+    
   end
   
   class TriangleError < StandardError
