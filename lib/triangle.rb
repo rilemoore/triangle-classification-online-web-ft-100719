48aebaea89de17c6cc80b76f53dbe6c0b0632877
Riley Moore <rilemoore@gmail.com>
2019-10-24 21:48:49 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,15 @@
 class Triangle
-  # write code here
+  
+  attr_accessor
+  
+  def initialize(side1, side2, side3)
+    
+  end
+  
+  class TriangleError < StandardError
+    
+  end
+  
+  
+  
 end
