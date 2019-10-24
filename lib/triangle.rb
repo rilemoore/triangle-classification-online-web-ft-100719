883432237f9c8e84a194dc40f0d6288a5193cb55
Riley Moore <rilemoore@gmail.com>
2019-10-24 22:07:35 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
 class Triangle
-  # write code here
+  
+  attr_accessor :side1, :side2, :side3
+  
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+  
+  def kind
+    triangle_array = [@side1, @side2, @side3].sort
+    
+    raise TriangleError, "Illegal Triangle" unless triangle_array[0] > 0
+    raise TriangleError, "Illegal Triangle" unless triangle_array[0] + triangle_array[1] > triangle_array[2]
+    
+    if(@side1 == @side2 && @side2 == @side3 && @side3 == @side1)
+      return :equilateral
+    elsif(@side1 == @side2 || @side2 == @side3 || @side3 == @side1 )
+      return :isosceles
+    else
+      return :scalene
+    end
+  end
+  
+  class TriangleError < StandardError
+    def message
+      "Illegal Triangle"
+    end
+  end
+  
+  
+  
 end
