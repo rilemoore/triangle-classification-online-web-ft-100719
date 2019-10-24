84d22d7693b3564762de24f3d7f13964c1a8533e
Riley Moore <rilemoore@gmail.com>
2019-10-24 21:58:00 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,6 +9,9 @@ class Triangle
   end
   
   def kind
+    
+    triangle_array = [:side1, :side2, :side3].sort
+    
     if(side1 == side2 && side2 == side3 && side3 == side1 && side1 > 0 && side2 > 0 && side3 > 0)
       return :equilateral
     end
