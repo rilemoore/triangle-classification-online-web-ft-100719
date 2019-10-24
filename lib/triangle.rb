3670b3d702ebecc5bef28b3b52b7fdf9aa8d8f27
Riley Moore <rilemoore@gmail.com>
2019-10-24 21:52:10 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,9 +1,17 @@
 class Triangle
   
-  attr_accessor :side1
+  attr_accessor :side1, :side2, :side3
   
   def initialize(side1, side2, side3)
-    
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+  
+  def kind
+    if(side1 == side2 && side2 == side3 && side3 == side1 && side1 > 0 && side2 > 0 && side3 > 0)
+      :equilateral
+    end
   end
   
   class TriangleError < StandardError
