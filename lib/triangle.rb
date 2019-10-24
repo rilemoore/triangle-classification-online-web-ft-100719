ed6cb9f9f6598c2546c48e04f9ee91bf2faa846e
Riley Moore <rilemoore@gmail.com>
2019-10-24 22:03:57 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,13 +12,14 @@ class Triangle
     
     triangle_array = [:side1, :side2, :side3].sort
     
-    if()
+    raise TriangleError, "Illegal Triangle" unless triangle_array[0] > 0
+    raise TriangleError, "Illegal Triangle" unless triangle_array[0] + triangle_array[1] > triangle_array[2]
     
-    if(@side1 == @side2 && @side2 == @side3 && @side3 == @side1 && @side1 > 0 && @side2 > 0 && @side3 > 0)
+    if(@side1 == @side2 && @side2 == @side3 && @side3 == @side1)
       return :equilateral
     end
     
-    if(side1 == side2 )
+    if(side1 == side2 || side2 == side3 || side3 == side1 )
     
   end
   
